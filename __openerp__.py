4c215afabec3e01227e692a90277994c70d73571
cmexia <carlos.lia.hmo@gmail.com>
2016-05-03 17:28:10 -0700

    Este es el primer commit para un odoo module!

---
+++ __openerp__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP.
+#    All Rights Reserved
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    'name': 'Modulo',
+    'suammary': """ Gestionar Entrenamientos!""", 
+    'version': '1.0',
+    #Las categorias las utilizamos para filtrar dentro de los modulos
+    'category': 'Test',
+    'description': """
+
+        Este modulo es concebido para hacer los primeros pasos en odoo version 8
+        -Agregar Vistas (XML)
+        -Cargar datos por default (DATA DEMO)
+        -Crear reglas de acceso (ACL)
+        -Procesos y objectos (Python)
+    """,
+    'author': 'Fernando Mexia',
+    'website':'www.typrefrigeracion.com.mx',
+    #Modulos de los cuales depende nuestro modulo, esto es como una especie de base en la que trabajaremos, siempre tendremos almenos
+    #la dependencia base ( modulo core)!
+    'depends': ['base'],
+    'data': [
+        #aqui por ejemplo podemos insertar un archivo .csv con las ACL que nos ayudaran a restringir acciones en nuestro modulo
+        #asi como filtrar usuarios  'security/ir.model.access.csv',
+        #Tambien podemos añadir un xml con datos por default    'template.xml',
+                ],
+    'update_xml': [
+        #'vista.xml',
+        ],
+    #Estos son datos que se cargan solamente si aplicamos el flag de instalar datos de demostracion al crear una BD nueva        
+    'demo': [
+        #'demo.xml',
+    ],
+    'installable': True,
+    'active': False,
+    'certificate' : False,
+}
---
+++ model/open_academy.py
@@ -0,0 +1,38 @@
+###########################################################################
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP.
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Carlos Fernando Mexia
+#    Planified by: Moylop
+#    Audited by:
+# "les piratie este encabezado, me parecio bueno"
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+from openerp import fields, models
+
+'''
+    Este modulo nos va crear la vista principal de nuestro modulo de demo! openacademy. com!!
+
+ '''
+
+class curso(models.Model):
+    "course module"
+    _name = 'open_academy.course' ## modelo llamado open academy para el curso tecnico de moylop
+
+    name = fields.Char(string='Title', required= True),
+    description = fiedls.Text(string='Description'),
