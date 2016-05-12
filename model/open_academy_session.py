2d52d6f301ae2c8c13def2a6bd576868c4981231
Nhomar Hernández <nhomar@vauxoo.com>
2016-05-12 14:00:16 -0500

    Session with create date.

--- model/open_academy_session.py
+++ model/open_academy_session.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from openerp import api,fields, models
 
+class SessionPartner(models.Model):
+    _name = "open_academy.session.partner"
+
+    partner_id = fields.Many2one('res.partner')
+    session_id = fields.Many2one('open_academy.session')
+    create_date = fields.Datetime()
+
 class Session(models.Model):
     _name = "open_academy.session"
 
@@ -10,19 +17,20 @@ class Session(models.Model):
     seats = fields.Integer(string="Numero de asientos en el curso")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')#podemos aplicar store=True
     instructor_id = fields.Many2one('res.partner',
-    								string="Instructor",
-    								on_delete="set null",
-    								index=True,
+                                    string="Instructor",
+                                    on_delete="set null",
+                                    index=True,
                                     domain=["|",
                                     ("instructor","=","True"),
                                     ("category_id","ilike","Teacher")])
                                     ##agregamos el dominio para que solo nos de a seleccionar partners que estan marcados
                                     ##como instructores o que contienen un tag teacher, Notese que utilizamos la notacion polaca
     course_id = fields.Many2one('open_academy.course',
-    							on_delete="cascade",
-    							string="Curso",
-    							requied=True)
-    attendes_ids = fields.Many2many('res.partner', string="Attendes")
+                                on_delete="cascade",
+                                string="Curso",
+                                requied=True)
+    attendes_ids = fields.Many2many('res.partner', relation='open_academy.session.partner',
+                                    column1='session_id', column2='partner_id', string="Attendes")
 
     @api.one #para que entre a cada uno de los registros
     @api.depends('seats','attendes_ids') #de que campos depende para llevar acabo la def
