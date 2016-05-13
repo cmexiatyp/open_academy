136c963dbbf32ae704808d9e45b75a78876b7705
cmexia <cmexia@typrefrigeracion.com>
2016-05-13 10:56:03 -0700

    [REF], quitamos class que no fue de utilidad y de paso modificamos el pylint

--- model/open_academy_session.py
+++ model/open_academy_session.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 from openerp import api,fields, models
 
-class SessionPartner(models.Model):
-    _name = "open_academy.session.partner"
-
-    partner_id = fields.Many2one('res.partner')
-    session_id = fields.Many2one('open_academy.session')
-    create_date = fields.Datetime()
 
 class Session(models.Model):
     _name = "open_academy.session"
 
     name = fields.Char(requied=True)## cuando no defines un string, por default aplica formato al nombre de la variable Name quedaria
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today())
     duration = fields.Float(digits=(6,2), help="duracion en dias")
     seats = fields.Integer(string="Numero de asientos en el curso")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')#podemos aplicar store=True
@@ -29,8 +23,11 @@ class Session(models.Model):
                                 on_delete="cascade",
                                 string="Curso",
                                 requied=True)
-    attendes_ids = fields.Many2many('res.partner', relation='open_academy.session.partner',
-                                    column1='session_id', column2='partner_id', string="Attendes")
+    attendes_ids = fields.Many2many('res.partner',
+                                    string="Attendes")
+  #                                 relation='open_academy_session_partner',
+  #                                 column1='session_id', column2='partner_id', string="Attendes")
+    active = fields
 
     @api.one #para que entre a cada uno de los registros
     @api.depends('seats','attendes_ids') #de que campos depende para llevar acabo la def
